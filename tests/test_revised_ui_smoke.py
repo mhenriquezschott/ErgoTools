a600be99994caa8e43349275b37a7e419ce8ca0e
Mauricio Henriquez <buhochileno@gmail.com>
2026-07-22 14:55:01 -0400

    add ErgoTools logo to main header

--- tests/test_revised_ui_smoke.py
+++ tests/test_revised_ui_smoke.py
@@ -33,6 +33,9 @@ parent.resize(1550, 1015)
 parent.show()
 app.processEvents()
 parent.grab().save("/tmp/main_damage_over_two.png")
+brand_logo = parent.findChild(QLabel, "brandLogo")
+assert brand_logo is not None
+assert brand_logo.pixmap() is not None and not brand_logo.pixmap().isNull()
 
 
 class CameraDirectorProbe:
